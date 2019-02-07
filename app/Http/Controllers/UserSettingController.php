45a4920f3b2791889efa21ada5e2fdd4295cee22
RitaJulianti <ritajulianti2002@gmail.com>
2019-02-07 18:21:56 +0700

    tokap eps 8

--- app/Http/Controllers/UserSettingController.php
+++ app/Http/Controllers/UserSettingController.php
@@ -12,11 +12,18 @@ class UserSettingController extends Controller
 {
 	public function form()
 	{
-		$data = user::where('id',Auth::id())->first();
+		$data = User::where('id',Auth::id())->first();
 		return view('admin.pages.user.setting',['dt'=>$data]);
 	}	
-	public function update()
+	public function update(Request $req)
 	{
-		return"Fungsi Update";
+		$id = Auth::id();
+		\Validator::make($req->all(), [
+			'name'=>'required|between:3,100',
+			'email'=>'required|email|unique:users,email,'.$id,
+			'password'=>'nullable|min:6',
+			'repassword'=>'same:password',
+		])->Validate();
+		return "Fungsi Update";
 	}
 }
